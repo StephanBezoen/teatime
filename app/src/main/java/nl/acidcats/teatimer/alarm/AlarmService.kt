10037a8dfe6fb27612405aa8e1d5f3d8bd5c3976
Stephan Bezoen <stephan@mediamonks.com>
2022-03-03 21:38:15 +0100

    Some cleanup

--- app/src/main/java/nl/acidcats/teatimer/alarm/AlarmService.kt
+++ app/src/main/java/nl/acidcats/teatimer/alarm/AlarmService.kt
@@ -7,7 +7,6 @@ import android.content.Intent
 import android.os.IBinder
 import android.widget.Toast
 import androidx.annotation.StringRes
-import com.github.ajalt.timberkt.Timber
 import nl.acidcats.teatimer.R
 import nl.acidcats.teatimer.ui.TeaTimeActivity
 import nl.acidcats.teatimer.util.*
@@ -60,34 +59,21 @@ class AlarmService : Service(), KoinComponent {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Timber.d { "Intent action = ${intent?.action}" }
-
-        return when (intent?.action) {
+        when (intent?.action) {
             StartAction.value -> {
                 startTimer(
                     minutes = intent.getIntExtra(DurationKey.value, 5),
                     widgetId = intent.getIntExtra(WidgetIdKey.value, 0)
                 )
-
-                START_STICKY
             }
-            CancelAction.value -> {
-                cancelTimer()
-
-                START_NOT_STICKY
-            }
-            DoneAction.value -> {
-                onTimerDone()
-
-                START_NOT_STICKY
-            }
-            else -> START_NOT_STICKY
+            CancelAction.value -> cancelTimer()
+            DoneAction.value -> onTimerDone()
         }
+
+        return START_NOT_STICKY
     }
 
     private fun startTimer(minutes: Int, widgetId: Int) {
-        Timber.d { "Starting timer for $minutes minutes" }
-
         val endTimeMs = Calendar.getInstance().apply { add(Calendar.MINUTE, minutes) }.timeInMillis
 
         storageHelper.alarmState = AlarmState(true, endTimeMs)
@@ -116,11 +102,7 @@ class AlarmService : Service(), KoinComponent {
     }
 
     private fun onTimerDone() {
-        storageHelper.alarmState = StoppedAlarm
-
         ScreenUtil.wakeScreen(this, 2.seconds)
-
-        NotificationUtil.cancelNotification(this, 1)
 
         NotificationUtil.showNotification(
             context = this,
@@ -136,17 +118,21 @@ class AlarmService : Service(), KoinComponent {
 
         showToast(R.string.tea_done_title, Toast.LENGTH_LONG)
 
-        stopSelf()
+        onAlarmStopped()
     }
 
     private fun cancelTimer() {
         alarmHelper.stopAlarm()
 
+        showToast(R.string.timer_stopped)
+
+        onAlarmStopped()
+    }
+
+    private fun onAlarmStopped() {
         NotificationUtil.cancelNotification(this, 1)
 
         storageHelper.alarmState = StoppedAlarm
-
-        showToast(R.string.timer_stopped)
 
         stopSelf()
     }
